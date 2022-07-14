0dac208f11a40e47f5dbd8e6c26b3a2d690cd4da
AntonioSabella <asabella6@gmail.com>
2022-07-14 19:06:23 +0200

    4-fix-apartment-seeder close #7

--- database/seeds/ApartmentSeeder.php
+++ database/seeds/ApartmentSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+use App\Models\Apartment;
 
 class ApartmentSeeder extends Seeder
 {
@@ -9,8 +12,16 @@ class ApartmentSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 20; $i++) {
+            $apartment = new Apartment;
+            $apartment->title = $faker->sentence(4);
+            $apartment->slug = Str::slug($apartment->title, '-');
+            $apartment->thumb = $faker->imageUrl(600, 400, 'thumb_apartment', true, 'thumb', true, 'jpg');
+            $apartment->description = $faker->text(500);
+            $apartment->address = $faker->address();
+            $apartment->save();
+        }
     }
 }
