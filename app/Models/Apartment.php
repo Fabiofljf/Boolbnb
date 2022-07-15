30ec2607a5caaf5001bad48e396a27f48543eef9
Belloni-Marco-Camillo <96737807+Belloni-Marco-Camillo@users.noreply.github.com>
2022-07-15 12:33:26 +0200

    add user apt relation

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Apartment extends Model
 {
-    //
+    /**
+     * Get the user that owns the Apartment
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- public/js/app.js
+++ public/js/app.js
@@ -53429,8 +53429,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Applications/MAMP/htdocs/Laravel/progetto-finale/boolbnb/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/Laravel/progetto-finale/boolbnb/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\MAMP\htdocs\progetto finale\Boolbnb\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\MAMP\htdocs\progetto finale\Boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
