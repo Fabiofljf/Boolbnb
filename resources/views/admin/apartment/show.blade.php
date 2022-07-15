4a0c9b42ce4184710c1b0d30d91c5156af6a9655
muccidavide <davidemucci0804@gmail.com>
2022-07-15 18:09:01 +0200

    8-CRUD-part-II close #15

---
+++ resources/views/admin/apartment/show.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <h2>{{$apartment->title}}</h2>
+        <p>{{$apartment->address}}</p>
+        <div class="apartment-thumb">
+            <img src="{{$apartment->thumb}}" alt="cover of {{$apartment->title}}">
+        </div>
+        <div class="apartment-description mt-3">
+            <h3>Description:</h3>
+            {{$apartment->description}}
+        </div>
+        <div class="apartment-details mt-3">
+            <h3>Details:</h3>
+            <ul>
+                <li>Rooms Numbers: {{$apartment->rooms}}</li>
+                <li>Beds Numbers: {{$apartment->beds}}</li>
+                <li>Baths Numbers: {{$apartment->baths}}</li>
+                <li>Sqm: {{$apartment->sqm}}</li>
+            </ul>
+        </div>
+
+        <div class="apartment-services mt-3">
+            <h3>Services:</h3>
+            <ul>
+                @forelse($apartment->services as $service)
+                <li>{!! $service->icon !!} {{$service->name}}</li> 
+                @empty
+                <li>No Serivices 😪</li>
+                @endforelse
+            </ul>
+        </div>
+        </div>
+        <div class="apartment-publicity mt-3">
+        <h3>Publicity:</h3>
+            <ul>
+                @forelse($apartment->publicities as $publicity)
+                <li>{!! $publicity->icon !!} {{$publicity->name}}</li>
+                @empty
+                <li>No Publicity 😪</li>
+                @endforelse
+            </ul>
+        </div>
+
+       
+
+
+
+
+    </div>
+</div>
+
+
+
+@endsection
