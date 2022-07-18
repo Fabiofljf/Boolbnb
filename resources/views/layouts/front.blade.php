3cd006278ea34f4b98830bc14714ea8d5b69ad04
FabioLaForesta <fabiolaforesta@icloud.com>
2022-07-18 16:05:53 +0200

    front

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -23,54 +23,65 @@
 
 <body>
 
-<header>
-<nav class="navbar navbar-expand-md navbar-dark bg_grey shadow-sm">
-            <a class="navbar-brand p-1" href="{{ url('/') }}">Boolbnb</a>
-            <!-- /logo -->
+    <header>
+        <nav class="navbar navbar-expand-md navbar-dark bg_grey shadow-sm">
             <div class="container">
-                <h4 class="navbar-brand mb-0">Bentornato {{ Auth::user()->name ?? '' }}</h4>
-                <!-- /name user -->
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <!-- /btn to drowdown -->
-                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto">
-                        @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                        @endif
-                        @else
+                <div class="row align-items-center">
+                    <div class="col">
+                        <a class="navbar-brand p-1" href="{{ url('/') }}">Boolbnb</a>
+                        <!-- /logo-->
+                    </div>
+                    <!-- /.col sx-->
+                    <div class="col text-light">
+                        search
+                    </div>
+                    <!-- /.col -->
+                    <div class="col">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <!-- /btn to drowdown -->
+                        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                            <ul class="navbar-nav ml-auto">
+                                @guest
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                                @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                                @endif
+                                @else
 
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                                </svg>
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="{{ route('admin.home') }}">Admin</a></li>
-                                <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a></li>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
+                                <li class="nav-item dropdown d-flex align-items-center">
+                                    <h4 class="navbar-brand mb-0">Bentornato {{ Auth::user()->name ?? '' }}</h4>
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                                        </svg>
+                                    </a>
+                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <li><a class="dropdown-item" href="{{ route('admin.home') }}">Admin</a></li>
+                                        <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a></li>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </ul>
+                                </li>
+                                @endguest
                             </ul>
-                        </li>
-                        @endguest
-                    </ul>
+                        </div>
+                        <!-- /authentication Links -->
+                    </div>
+                    <!-- /.col dx-->
                 </div>
-                <!-- /authentication Links -->
             </div>
         </nav>
         <!-- /navbar -->
 
-</header>
+    </header>
 
     @yield('content')
 
