257b46c1faa0947e9f4b751d8ec25c8645e6890d
MirkOrlando <mirko@orlando-m.it>
2022-07-19 19:02:34 +0200

    create map in apartment.show

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,5 @@
+
+
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -6,7 +8,26 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+/* window.Vue = require('vue'); */
+
+import tt from '@tomtom-international/web-sdk-maps';
+
+const map = tt.map({
+    key: 'wwBjO0iyrGBDWYAR81J5EY7D4Y0HJGQj',
+    container: 'map'
+});
+
+/* import tt from '@tomtom-international/web-sdk-services';
+
+tt.services.copyrights({
+    key: 'ZKEljqh55cAJVmD8GpeG3iI4JmV5HEDm'
+})
+    .then(function (results) {
+        console.log('Copyrights', results);
+    })
+    .catch(function (reason) {
+        console.log('Copyrights', reason);
+    }); */
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +40,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+/* Vue.component('example-component', require('./components/ExampleComponent.vue').default); */
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -27,6 +48,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
+/* const app = new Vue({
     el: '#app',
-});
+});  */
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,22 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-
-<section id="dashboard">
-    <div class="card">
-        <div class="card-header">
-            <h2>{{ __('Dashboard') }}</h2>
-        </div>
-        <div class="card-body">
-            @if (session('status'))
-            <div class="alert alert-success" role="alert">
-                {{ session('status') }}
+    <section id="dashboard">
+        <div class="card">
+            <div class="card-header">
+                <h2>{{ __('Dashboard') }}</h2>
             </div>
-            @endif
+            <div class="card-body">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
 
-            {{ __('You are logged in!') }}
+                {{ __('You are logged in!') }}
+            </div>
         </div>
-    </div>
-</section>
-
+    </section>
 @endsection
