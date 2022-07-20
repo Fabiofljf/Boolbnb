d86c4fd5e1a446f5c9c64b1c8dbfb5e7ea899bb1
Belloni-Marco-Camillo <96737807+Belloni-Marco-Camillo@users.noreply.github.com>
2022-07-20 09:30:07 +0200

    add.-width-img

--- resources/views/admin/apartment/index.blade.php
+++ resources/views/admin/apartment/index.blade.php
@@ -30,7 +30,7 @@
                                 <tr>
                                     <td scope="row">{{ $apartment->id }}</td>
                                     <td>{{ $apartment->title }}</td>
-                                    <td> <img src="{{ asset('/storage/' . $apartment->thumb)}}" alt="thumb of {{$apartment->title}}"></td>
+                                    <td> <img style="max-width: 200px" src="{{ asset('/storage/' . $apartment->thumb)}}" alt="thumb of {{$apartment->title}}"></td>
                                     <td>{{ $apartment->address }}</td>
                                     <td>
                                         {{ Str::limit($apartment->description, 200) }}
