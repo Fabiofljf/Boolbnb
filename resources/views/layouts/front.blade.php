157199c14e7b80de4997f47f971108534def89be
MirkOrlando <mirko@orlando-m.it>
2022-07-28 18:43:15 +0200

    fix header

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -52,23 +52,41 @@
                         </li>
                         <li class="right">
                             <ul>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="dropdownId"
-                                        data-bs-toggle="dropdown" aria-haspopup="true"
-                                        aria-expanded="false">{{ Auth::user()->name ?? '' }}
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                            fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                                            <path fill-rule="evenodd"
-                                                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                                        </svg>
-                                    </a>
-                                    <ul class="dropdown-menu" aria-labelledby="dropdownId">
-                                        <li><a class="dropdown-item" href="{{ route('admin.home') }}">Admin</a></li>
-                                        <li><a class="dropdown-item" href="{{ route('logout') }}"
-                                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Esci</a>
+                                @guest
+                                    <ul class="d-flex flex-column flex-sm-row">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="{{ route('login') }}">Accedi</a>
+                                        </li>
+                                        @if (Route::has('register'))
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="{{ route('register') }}">Registrati</a>
+                                            </li>
+                                        @endif
                                     </ul>
-                                </li>
+                                @else
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="dropdownId"
+                                            data-bs-toggle="dropdown" aria-haspopup="true"
+                                            aria-expanded="false">{{ Auth::user()->name ?? '' }}
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                                                <path fill-rule="evenodd"
+                                                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                                            </svg>
+                                        </a>
+                                        <ul class="dropdown-menu" aria-labelledby="dropdownId">
+                                            <li><a class="dropdown-item" href="{{ route('admin.home') }}">Admin</a></li>
+                                            <li><a class="dropdown-item" href="{{ route('logout') }}"
+                                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Esci</a>
+                                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                                    class="d-none">
+                                                    @csrf
+                                                </form>
+
+                                        </ul>
+                                    </li>
+                                @endguest
                             </ul>
                         </li>
                     </ul>
