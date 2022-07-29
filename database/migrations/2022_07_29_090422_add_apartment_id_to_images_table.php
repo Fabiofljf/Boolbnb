f428ac59ee2c0e4e7f1b1e7fbccc01d48e555553
AntonioSabella <asabella6@gmail.com>
2022-07-29 12:38:26 +0200

    fix migration and layout

--- database/migrations/2022_07_29_090422_add_apartment_id_to_images_table.php
+++ database/migrations/2022_07_29_090422_add_apartment_id_to_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddArticleIdToImagesTable extends Migration
+class AddApartmentIdToImagesTable extends Migration
 {
     /**
      * Run the migrations.
